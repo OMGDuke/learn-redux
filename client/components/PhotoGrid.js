fa424fccc40692a372fb2f56e12f72b7603c4f7d
OMGDuke <alex@omgduke.com>
2018-06-10 10:17:07 +0900

    Add basic app layout and router

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div className="photo-grid">
+      I'm a grid
+    </div>
+  )
+}
---
+++ client/components/Single.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div className="single-photo">
+      I'm a photo
+    </div>
+  )
+}
